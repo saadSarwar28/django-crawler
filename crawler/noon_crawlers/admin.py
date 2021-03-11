d8ef76b8a64404720fd914a0a99a8d0dc4523c07
saadSarwar28 <saad_sarwar22@hotmail.com>
2021-03-12 04:50:40 +0500

    functionality enhanced with email

--- crawler/noon_crawlers/admin.py
+++ crawler/noon_crawlers/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 class ListAdminMixin(object):
     def __init__(self, model, admin_site):
         self.list_display = [field.name for field in model._meta.fields]
-        self.list_filter = [field.name for field in model._meta.fields]
+        # self.list_filter = [field.name for field in model._meta.fields]
         self.search_fields = [field.name for field in model._meta.fields]
         super(ListAdminMixin, self).__init__(model, admin_site)
 
