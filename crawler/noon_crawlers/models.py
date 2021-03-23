4c9fdd3791b5fe3f762d28a8fd2d7c68aa8d18af
saadSarwar28 <saad_sarwar22@hotmail.com>
2021-03-23 20:40:21 +0500

    bug fixes

--- crawler/noon_crawlers/models.py
+++ crawler/noon_crawlers/models.py
@@ -34,7 +34,7 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.sku
+        return self.product_title
 
 
 class Day(models.Model):
