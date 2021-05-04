7772ba95a309ebc3e39b3e6cefcd06135a8b8da8
AsadJamali88 <lancer0@hotmail.com>
2021-05-05 02:02:30 +0500

    google api login through service account

--- crawler/noon_crawlers/google_drive_api_login.py
+++ crawler/noon_crawlers/google_drive_api_login.py
@@ -3,33 +3,37 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-
+from google.oauth2 import service_account
 SCOPES = ['https://www.googleapis.com/auth/drive']
+SERVICE_ACCOUNT_FILE = 'crawler/service-account.json'
 
 def login_google():
     try:
         """Shows basic usage of the Drive v3 API.
         Prints the names and ids of the first 10 files the user has access to.
         """
-        creds = None
+        # creds = None
         # The file token.pickle stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
-                creds = pickle.load(token)
+        # if os.path.exists('token.pickle'):
+        #     with open('token.pickle', 'rb') as token:
+        #         creds = pickle.load(token)
         # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file('crawler/service-account.json', SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.pickle', 'wb') as token:
-                pickle.dump(creds, token)
+        # if not creds or not creds.valid:
+        #     if creds and creds.expired and creds.refresh_token:
+        #         creds.refresh(Request())
+        #     else:
+        #
+        #         flow = InstalledAppFlow.from_client_secrets_file('crawler/service-account.json', SCOPES)
+        #         creds = flow.run_local_server(port=0)
+        #     Save the credentials for the next run
+            # with open('token.pickle', 'wb') as token:
+            #     pickle.dump(creds, token)
 
-        service = build('drive', 'v3', credentials=creds)
+        credentials = service_account.Credentials.from_service_account_file(
+            SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+        service = build('drive', 'v3', credentials=credentials)
         return service
         # Call the Drive v3 API
         # results = service.files().list(
